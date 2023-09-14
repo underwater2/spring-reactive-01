26523f9e8fca747e1b09c8d488b4afaddfaab368
underwater2 <shaian2@naver.com>
2023-09-14 16:32:43 +0900

    first commit

---
+++ src/main/java/com/example/springreactive01/SpringReactive01Application.java
@@ -0,0 +1,13 @@
+package com.example.springreactive01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactive01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactive01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springreactive01/SpringReactive01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreactive01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactive01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
