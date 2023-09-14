573e5e51236810872f197f240a08ad174b8aebd9
underwater2 <shaian2@naver.com>
2023-09-14 16:35:55 +0900

    섹션3.2 듣는중

---
+++ src/main/java/com/example/section02/class01/HelloReactorExample.java
@@ -0,0 +1,13 @@
+package com.example.section02.class01;
+
+import com.example.utils.Logger;
+import reactor.core.publisher.Flux;
+
+public class HelloReactorExample {
+    public static void main(String[] args) {
+        Flux<String> sequence = Flux.just("Hello", "Reactor");
+        sequence
+                .map(data -> data.toLowerCase())
+                .subscribe(data -> Logger.onNext(data));
+    }
+}
